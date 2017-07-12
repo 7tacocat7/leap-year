0051dcb03c7f863a4c7a055b8dc403ec438a6247
Bart Webber and Carson Canales <bart@>
2017-07-12 11:22:06 -0700

    add commit to code to understande

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
     return true;
@@ -9,13 +10,22 @@ var leapYear = function(year) {
 
 
 
-
+// user interface logic
 $(document).ready(function(){
   $("form#leap-year").submit(function(event){
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+// code below says in laymen's terms - if the result is equal to false - than displan the text "not" in the span class .not for the output text on the webpage.
+    if(!result) {                 // same as writing if (result === false)
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+// the code establishes result div to show the id result after the submit click function is activated
+    $("#result").show();
 
 
   });
